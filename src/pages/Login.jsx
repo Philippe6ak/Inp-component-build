d98b80291974bc689284ee9522087b26f80db0e9
Philippe6ak <philippe.siaka@gmail.com>
2026-04-02 12:00:57 +0000

    Pages transition

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,26 +1,14 @@
-import styled from "styled-components";
-import LoginForm from "../features/authentication/LoginForm";
-import Heading from "../ui/Heading";
-import Logo from "../ui/Logo";
-import CabinTable from "../features/cabins/CabinTable";
-
-const LoginLayout = styled.main`
-  min-height: 100vh;
-  display: grid;
-  grid-template-columns: 48rem;
-  align-content: center;
-  justify-content: center;
-  gap: 3.2rem;
-  background-color: var(--color-grey-50);
-`;
+import LoginForm from '../features/authentication/LoginForm';
+import Heading from '../ui/Heading';
+import Logo from '../ui/Logo';
 
 function Login() {
   return (
-    <LoginLayout>
+    <main className="min-h-screen grid grid-cols-[48rem] content-center justify-center gap-[3.2rem] bg-grey-50">
       <Logo />
       <Heading as="h4">Log into your account</Heading>
       <LoginForm />
-    </LoginLayout>
+    </main>
   );
 }
 
